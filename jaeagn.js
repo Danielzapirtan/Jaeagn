a35f5108ab13e34b697e88f024c7ec07da037c5c
Antoniu-Daniel Zăpîrțan <antoniudanielzapirtan@gmail.com>
2024-09-23 08:17:09 +0000

    m

--- jaeagn.js
+++ jaeagn.js
@@ -375,6 +375,7 @@ function removeFirstChild(element) {
 
 function updateTable(display) {
   const table = createTable(JSON.stringify(display));
+  removeFirstChild(output);
   output.appendChild(table);
 }
 
