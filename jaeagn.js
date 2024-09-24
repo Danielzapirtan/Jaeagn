4c1f3f79e7a0287addafd9dfb6ddc2cd673f91b8
Antoniu-Daniel Zăpîrțan <antoniudanielzapirtan@gmail.com>
2024-09-24 16:45:10 +0000

    m

--- jaeagn.js
+++ jaeagn.js
@@ -204,5 +204,5 @@ worker.onmessage = (event) => {
 };
 
 function jana() {
-  worker.postMessage({ start: JSON.stringify({gstart, stm}) }); // Example input data
+  worker.postMessage({ start: JSON.stringify({gstart, stm}) });
 }
