17985e66f7767ae4c9fdc03b49d5e06489e4bd3b
Danielzapirtan <137394552+Danielzapirtan@users.noreply.github.com>
2024-09-28 04:54:48 +0300

    Update jaeagn.js

--- jaeagn.js
+++ jaeagn.js
@@ -50,8 +50,8 @@ function drawPieces(board14) {
 
 function toUnicodePiece(piece1) {
 	switch (piece1) {
-		'K': return String.fromCharCode(0x2654);
-		default: return piece1;
+	case 'K': return String.fromCharCode(0x2654);
+	default: return piece1;
 	}
 }
 
